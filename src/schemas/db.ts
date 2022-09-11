867c5dfe96a2a2d8e64bbe6552c728905b8d4ef9
synysterzv <greendayfanj@gmail.com>
2022-09-11 23:27:10 +0000

    fix: ts is dumb

--- src/schemas/db.ts
+++ src/schemas/db.ts
@@ -19,7 +19,7 @@ export class DB {
 
   async starboardGet(id: string) {
     const res = await this.sql<Starboard[]>`SELECT * FROM starboard WHERE oid=${id}`;
-    return res[0]
+    return res[0] || undefined;
   }
 
   starboardAdd(id: string, oid: string) {
